6348e9f18ddef2742419bb2a603cc2db26005ccb
Hilary <watson.hilary@gmail.com>
2019-01-16 16:57:37 -0500

    logout function implemented

--- express_server.js
+++ express_server.js
@@ -105,4 +105,10 @@ app.post("/login", (req,res) => {
   res.redirect(`/urls`);        // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/logout", (req,res) => {
+  res.clearCookie('username');
+  res.redirect(`/urls`);        // Respond with 'Ok' (we will replace this)
+});
+
+
 
